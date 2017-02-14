755d5ec87781c0501411d8c7ea7e4e1c072738bb
SunSet0418 <cds07016@naver.com>
2017-02-15 00:55:14 +0900

    set header

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 mongoose.connect("mongodb://localhost:28001/login", function(err){
   if(err){
     console.log("DB Error!");
